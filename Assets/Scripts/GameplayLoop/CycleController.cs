e24c0573f6b74622842c6207aef69843d6ab8496
Hayley Davies <cd230099@falmouth.ac.uk>
2021-11-02 16:06:42 +0000

    Created a GameController and CycleController script
    - Wanting to use UnityEvents to control the cycles - makes it more modular
    - Can't do a lot of work on this functionality until other scripts have made, such as the level info etc

---
+++ Assets/Scripts/GameplayLoop/CycleController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TrojanMouse.GameplayLoop {
+    [CreateAssetMenu(fileName = "CycleInfo", menuName = "ScriptableObjects/GameplayLoop/CycleController", order = 1)]
+    public class CycleController : ScriptableObject {
+        public UnityEvent cycleController;
+        public void AddNanaBetsys(int quantityToAdd) {
+            GameController.nanaBetsyCount += quantityToAdd;
+        }
+    }
+}
---
+++ Assets/Scripts/GameplayLoop/GameController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrojanMouse.GameplayLoop {
+    public class GameController : MonoBehaviour {
+        public List<CycleController> cycles;
+
+        public int currentCycle = 0;
+
+        public static int nanaBetsyCount = 0;
+
+        // Start is called before the first frame update
+        void Start() { }
+
+        // Update is called once per frame
+        void Update() { }
+
+        public void StartNewCycle() {
+            cycles[currentCycle].cycleController.Invoke();
+            currentCycle++;
+            if (currentCycle >= cycles.Count) {
+                currentCycle = 0;
+            }
+        }
+
+        /// <summary>
+        /// GET THE NEXT LEVEL AND START THE PLAYFIELD WITH IT
+        /// </summary>
+        public void StartLevel() {
+
+        }
+    }
+}
