939e009cb3403dfcced0dabed24bd383f0f20549
Hayley Davies <cd230099@falmouth.ac.uk>
2021-11-04 01:14:12 +0000

    Made ScenePreloading more streamlined
    - Moved the StartAsync function directly into the PreloadScene function

--- Assets/Scripts/Generics/ScenePreloading.cs
+++ Assets/Scripts/Generics/ScenePreloading.cs
@@ -13,23 +13,14 @@ namespace TrojanMouse.Generic {
         /// </summary>
         /// <param name="scene">The scene to preload.</param>
         public static IEnumerator PreloadScene(Scene scene) {
-            StartAsync(scene);
+            asyncLoad = SceneManager.LoadSceneAsync(scene.buildIndex);
+
+            asyncLoad.allowSceneActivation = false;
 
             while (!asyncLoad.isDone) {
                 yield return null;
             }
         }
-
-        /// <summary>
-        /// Start the Async process.
-        /// </summary>
-        /// <param name="scene">The scene to async load.</param>
-        public static void StartAsync(Scene scene) {
-            asyncLoad = SceneManager.LoadSceneAsync(scene.buildIndex);
-
-            asyncLoad.allowSceneActivation = false;
-        }
-
         /// <summary>
         /// Switch to the scene that has been preloaded.
         /// </summary>
