27c3b7ac9500447cf14dd9e5657322a284f6e78d
Josh Thompson <jt244345@falmouth.ac.uk>
2021-11-09 17:07:50 +0000

    Began working on refactoring my region code

---
+++ Assets/Scripts/RegionHandling/Region.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+
+
+/* 
+--// HOW TO USE \\--
+Listening to feedback from my previous system, i was asked to try make it more minimal and ease of access for designers
+and therefore i am trying to use built-in unity feature such as colliders to measure boundaries such as litter spawning regions and more
+
+- Move this script onto a region object and it should generate a collider for you? Preferably a box collider - You should be able to change it to another type if you wish!
+- Re-scale the collider to the region size
+- Move the object to the desired position on the map
+- In inspector where this script is located, assign the correct variables to match the purpose of this region 
+
+- MAKE SURE THIS OBJECT COLLIDER IS SET TO TRIGGER!
+*/
+
+
+
+// DEVELOPED BY JOSH THOMPSON
+namespace TrojanMouse.RegionManagement{
+    [RequireComponent(typeof(Collider))] public class Region : MonoBehaviour{
+        [Header("Please open this script to see how to use it!")]
+        [SerializeField] RegionType type;
+        public RegionType Type{ get{ return type; } } // PROGRAMMERS ACCESS THIS VARIABLE -- Read only, returns the type of region this is
+
+        [SerializeField] Color32 debugColour = Color.red;
+        public Color32 DebugColour{ get{ return debugColour;} }
+
+        private void Start() => RegionHandler.current.PingRegions += PingRegion;
+        void PingRegion() => RegionHandler.current.AddRegion(this);
+
+        [Serializable] public enum RegionType{
+            NONE,
+            LITTER_REGION,
+            HOME
+        }
+    
+    
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = DebugColour;
+            Gizmos.DrawWireSphere(transform.position, .1f);
+       }
+    }
+}
