adc42f09ea7c2266a784753c745d15a5a3d812c3
Matthew Roberts <mr232432@falmouth.ac.uk>
2021-11-16 11:28:56 +0000

    Imported newer scripts
    imported all the newer scripts ahead of the refactor

--- Assets/Scripts/AI/Friendly.cs
+++ Assets/Scripts/AI/Friendly.cs
@@ -7,14 +7,12 @@ namespace TrojanMouse.AI.Behaviours
     public class Friendly : MonoBehaviour
     {
         // Start is called before the first frame update
-        void Start()
-        {
+        void Start() {
 
         }
 
         // Update is called once per frame
-        void Update()
-        {
+        void Update() {
 
         }
     }
