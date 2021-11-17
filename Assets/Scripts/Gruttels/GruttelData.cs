b341ae9ad300448399294a4b162c8d07393a8689
Hayley Davies <cd230099@falmouth.ac.uk>
2021-11-17 22:33:04 +0000

    Added the gruttel scriptable objects
    - Changed the Data to be a standard class (not scriptable object)
    - Changed how GruttelBaseType handles MeshFilters, this is how a GameObject which will be a prefab for the gruttel
    - Data stores the baseType, gruttelName, baseColor and overallStress
    - All enums for the gruttels are stored in the namespace TrojanMouse.Gruttel

--- Assets/Scripts/Gruttels/GruttelData.cs
+++ Assets/Scripts/Gruttels/GruttelData.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TrojanMouse.Gruttel
-{
-    public class Data
-    {
+namespace TrojanMouse.Gruttel {
+    public class Data {
         public GruttelBaseType baseType;
         public string gruttelName;
         public Color baseColor;
         public int overallStress;
 
-        public bool UpdateGruttelType(GruttelBaseType type)
-        {
-            try
-            {
+        public bool UpdateGruttelType(GruttelBaseType type) {
+            try {
                 baseType = type;
-            }
-            catch
-            {
+            } catch {
                 return false;
             }
 
