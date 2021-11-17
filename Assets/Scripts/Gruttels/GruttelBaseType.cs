0d77926bd90d8c9e99a86e7d0c1870b4bca64a6d
Hayley Davies <cd230099@falmouth.ac.uk>
2021-11-17 02:37:01 +0000

    Created basic scriptable object for the gruttel
    - Not keen on the current implementation, want to move into seperate classes and not nested ones

---
+++ Assets/Scripts/Gruttels/GruttelBaseType.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrojanMouse.Gruttel {
+    public class GruttelBaseType {
+        public MeshFilter model;
+    }
+
+}
