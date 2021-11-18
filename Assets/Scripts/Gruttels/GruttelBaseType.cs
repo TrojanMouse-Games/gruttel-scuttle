2157b5f3b4d95aa782c5dd22deccca4829cbe9ce
Hayley Davies <cd230099@falmouth.ac.uk>
2021-11-18 07:19:36 +0000

    Code for Gruttels is now formatted as per C# Code Formatting guide

--- Assets/Scripts/Gruttels/GruttelBaseType.cs
+++ Assets/Scripts/Gruttels/GruttelBaseType.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TrojanMouse.Gruttel {
+namespace TrojanMouse.Gruttel
+{
 
     [CreateAssetMenu(fileName = "GruttelBaseType", menuName = "ScriptableObjects/Gruttel/GruttelBaseType", order = 1)]
-    public class GruttelBaseType : ScriptableObject {
+    public class GruttelBaseType : ScriptableObject
+    {
         public GameObject character { get; }
         public Gruttel.Gender gender { get; }
         public Gruttel.Type type { get; }
 
-        public string GetName() {
+        public string GetName()
+        {
             return name;
         }
     }
