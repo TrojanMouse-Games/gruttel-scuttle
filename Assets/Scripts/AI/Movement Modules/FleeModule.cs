f01bdcbba1efed52fdc82284ba878c48b314e6e5
Matthew Roberts <mr232432@falmouth.ac.uk>
2021-11-23 13:05:18 +0000

    Created AIData, removed old variables from AIController and updated all scripts to use AIData
    Created AIData to hold most of the important HFSM data, all scripts now use the AIData class
    Created FleeModule, still need implementation

---
+++ Assets/Scripts/AI/Movement Modules/FleeModule.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeModule : MonoBehaviour
+{
+    // // TODO:- Make the transition back out of this state using a check to see if the AI has gotten far enough away from the target.
+    // // STRETCH:- Convert to use NavMeshHit to create a more dynamic, realistic looking flee. Maybe it will only try this more dynamic style once it's a set distance away.
+    // /// <summary>
+    // /// This function handles the fleeing of the AI. Forcing the AI to run away from its target/agressor. Reuses some variables.
+    // /// <para>Ignores the check for litter, as it doesn't make sense for them to care about litter when fleeing.</para>
+    // /// </summary>
+    // private void Flee(GameObject currentTarget, float wanderRadius)
+    // {
+    //     //CheckForLitter(); 
+
+    //     // Probably a better way to do this, but this is relatively simple.
+    //     float distance = Vector3.Distance(gameObject.transform.position, currentTarget.transform.position);
+    //     // Get the direction, then invert it
+    //     Vector3 dir = -(currentTarget.transform.position - transform.position);
+
+    //     if (distance <= wanderRadius)
+    //     {
+    //         // Increase speed
+    //         agent.speed *= 1.5f;
+    //         // Run away from target
+    //         agent.SetDestination(transform.position + dir.normalized);
+    //     }
+    //     else
+    //         agent.speed = 3.5f;
+    // }
+}
