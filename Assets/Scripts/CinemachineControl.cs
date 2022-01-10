aed7a7514c00cbfef98bfee604bfa622c07c9d1c
Cassy <EB247426@falmouth.ac.uk>
2022-01-10 11:18:12 +0000

    Updated script to add scroll zoom and prefabed camera objects for integration

--- Assets/Scripts/CinemachineControl.cs
+++ Assets/Scripts/CinemachineControl.cs
@@ -9,14 +9,15 @@ public class CinemachineControl : MonoBehaviour
     //by Cassy 
 
     [SerializeField]
-    private float speed;
-    float hMove;
+    private float camMoveSpeed = 1; 
+    [SerializeField]
+    private float camZoomSpeed = 1;
+    float hMove;float minPos;float maxPos;float startPos; float fov = 50; float scrollMove;float maxZoom = 30;float minZoom = 70;
+    //drag the scene's main v cam here
     public GameObject virtualCam;
-    CinemachineTrackedDolly dolly;
-    float minPos;
-    float maxPos;
-    float startPos;
+    CinemachineVirtualCamera vcamComponent; CinemachineTrackedDolly dolly;
     string sceneName;
+
 
     // Start is called before the first frame update
     void Start()
@@ -36,10 +37,14 @@ public class CinemachineControl : MonoBehaviour
                 minPos = 0; maxPos = 4; startPos = 2;
                 break;
         }
+        //fetches the virtual camera component
+        vcamComponent = virtualCam.GetComponent<CinemachineVirtualCamera>();
         //gets the dolly cam component
-        dolly = virtualCam.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>();
+        dolly = vcamComponent.GetCinemachineComponent<CinemachineTrackedDolly>();
         //sets to start position to 0
         dolly.m_PathPosition = startPos;
+        //sets the fov to the starting preset
+        vcamComponent.m_Lens.FieldOfView = fov;
     }
 
     // Update is called once per frame
@@ -47,6 +52,27 @@ public class CinemachineControl : MonoBehaviour
     {
         //gets horizontal input
         hMove = Input.GetAxis("Horizontal");
+        //gets scroll input
+        scrollMove = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollMove != 0f)
+        {
+            CamZoom();
+        }
+        //if movement input received
+        if (hMove != 0)
+        {
+            //function to move camera
+            CamMovement();
+        }
+    }
+    void CamMovement()
+    { 
+        //as long as if within range, moves camera along dolly
+        if (minPos <= dolly.m_PathPosition && dolly.m_PathPosition <= maxPos)
+        {
+            dolly.m_PathPosition += (hMove * camMoveSpeed * Time.deltaTime );
+        }
         //sets position to min/max if it goes out of bounds
         if (minPos > dolly.m_PathPosition)
         {
@@ -56,14 +82,22 @@ public class CinemachineControl : MonoBehaviour
         {
             dolly.m_PathPosition = maxPos;
         }
-        //movement input received
-        if (hMove != 0)
+    }
+    void CamZoom()
+    {
+        //as long as if within range, zooms camera
+        if (maxZoom <= vcamComponent.m_Lens.FieldOfView && vcamComponent.m_Lens.FieldOfView <= minZoom)
         {
-            //as long as if within range, moves camera along dolly
-            if (minPos <= dolly.m_PathPosition && dolly.m_PathPosition <= maxPos)
-            {
-                dolly.m_PathPosition += (hMove * speed * Time.deltaTime );
-            }
+            vcamComponent.m_Lens.FieldOfView -= (scrollMove * (camZoomSpeed*1000) * Time.deltaTime);
+        }
+        //sets zoom level to min/max if goes out of bounds
+        if (maxZoom > vcamComponent.m_Lens.FieldOfView)
+        {
+            vcamComponent.m_Lens.FieldOfView = maxZoom;
+        }
+        if (vcamComponent.m_Lens.FieldOfView > minZoom)
+        {
+            vcamComponent.m_Lens.FieldOfView = minZoom;
         }
     }
 }
