dd4a2aa2df3f250c0ecf2d14d5ad00b33aedcf00
mr232432 <mr232432@falmouth.ac.uk>
2022-02-25 17:42:30 +0000

    Rewrote the old broken main menu script.
    Fixed typo on eggs tutorial level.

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TrojanMouse.Menu
+{
+    /// <summary>
+    /// Controls the main menu. Need to reincorporate async loading.
+    /// </summary>
+    public class MainMenu : MonoBehaviour
+    {
+        public string sceneToPlay;
+
+        [Header("UI Elements")]
+        public GameObject playButton;
+        public GameObject levelPanel;
+
+        // to be used later.
+        AsyncOperation async;
+
+        #region  LEVEL_LOADING
+        public void SelectLevel(string levelToSelect)
+        {
+            sceneToPlay = levelToSelect;
+            FixUI();
+        }
+
+        /// <summary>
+        /// Loads a level, can either load an external passed in level OR whatevers selected in the Select level function.
+        /// </summary>
+        /// <param name="level">The level to load.</param>
+        public void LoadLevel(string alternateLevel)
+        {
+            if (alternateLevel == null)
+            {
+                SceneManager.LoadScene(sceneToPlay);
+            }
+            else
+            {
+                SceneManager.LoadScene(alternateLevel);
+            }
+        }
+        #endregion
+
+        #region OTHER_FUNCTIONS
+        /// <summary>
+        /// Fixes the UI to be correct, disables the level panel and enables the play button
+        /// </summary>
+        public void FixUI()
+        {
+            levelPanel.SetActive(false);
+            playButton.SetActive(true);
+        }
+
+        /// <summary>
+        /// Opens a URL in the default browser.
+        /// </summary>
+        /// <param name="url">The url to open</param>
+        public void OpenLink(string url)
+        {
+            Application.OpenURL(url);
+        }
+
+        /// <summary>
+        /// Quits the game.
+        /// </summary>
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        #endregion
+    }
+
+}
