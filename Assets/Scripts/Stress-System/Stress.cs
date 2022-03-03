e92d524794f7688dec834c56b8ea9b453fc48fe5
Josh Thompson <jt244345@falmouth.ac.uk>
2022-03-03 14:39:03 +0000

    Stress now counts down when reached the max, then enters lose screen

--- Assets/Scripts/Stress-System/Stress.cs
+++ Assets/Scripts/Stress-System/Stress.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using System.Collections;
 using TrojanMouse.RegionManagement;
+using UnityEngine.SceneManagement;
 
 // MADE BY JOSHUA THOMPSON
 namespace TrojanMouse.StressSystem
@@ -8,12 +10,14 @@ namespace TrojanMouse.StressSystem
     public class Stress : MonoBehaviour{
         public static Stress current;
         Region[] litterRegions = new Region[] { };
+        [HideInInspector] public bool isCountingDown;
 
         [HideInInspector] public float amountOfLitter;
 
         [Header("Settings")]
         [Tooltip("Time until this script will calculate stress again")] [SerializeField] float calculationCooldown; // TIME BETWEEN EACH CALCULATION FOR STRESS
-
+        [SerializeField] float maxStressCountdown;
+        float curCountdown;
         public float maxLitter;        
         [HideInInspector] public float Cooldown{
             get{
@@ -33,11 +37,27 @@ namespace TrojanMouse.StressSystem
         }        
 
         private void Update(){
+            #region STRESS COUNTDOWN
+            if(amountOfLitter >= maxLitter && !isCountingDown){
+                isCountingDown = true;
+                curCountdown = Time.time + maxStressCountdown;
+            }
+
+            if(Time.time >= curCountdown && isCountingDown){
+                isCountingDown = false;
+                if(amountOfLitter >= maxLitter){
+                    // switch scene
+                    SceneManager.LoadSceneAsync("LoseScreen");  
+                }                
+            }
+            #endregion
             if (litterRegions.Length > 0 || Region_Handler.current.GetRegions(Region.RegionType.LITTER_REGION) == null){ // SAFETY MEASURE TO ENSURE THAT THERE ARE REGIONS TO ACCESS AND THAT IT ONLY IS CALLED ONCE
                 return;
             }
             litterRegions = Region_Handler.current.GetRegions(Region.RegionType.LITTER_REGION); // POPULATES THE 'litterRegions' ARRAY WITH ALL REGIONS
         }
+
+        
 
         void UpdateStress(){
             if (litterRegions.Length > 0){ // SAFETY MEASURE TO MAKE SURE THIS IS POPULATED
@@ -48,6 +68,5 @@ namespace TrojanMouse.StressSystem
                 amountOfLitter = stress; // SET THE PUBLIC READABLE VALUE TO THE AMOUNT OF STRESS
             }
         }
-
     }
 }
