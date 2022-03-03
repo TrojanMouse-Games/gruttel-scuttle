52fc70ad01fed8c054cca2b0e59482b22bcee1a1
Otis HUll <oh249978@falmouth.ac.uk>
2022-03-03 13:23:52 +0000

    Litter spawn sounds
    Tinkering with the fequency of sounds and also volume

--- Assets/Scripts/Gameloop-Revamped/Gameloop/Behaviours/ReadyStage/Intermission.cs
+++ Assets/Scripts/Gameloop-Revamped/Gameloop/Behaviours/ReadyStage/Intermission.cs
@@ -2,20 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using FMODUnity;
 
 namespace TrojanMouse.GameplayLoop{ 
     public class Intermission : GLNode{
         public float maxDuration, remainingDuration;
         public Image imageUI;
-        public StudioGlobalParameterTrigger
         public Intermission(float duration, Image imageUI = null){
             this.maxDuration = duration;
             this.remainingDuration = duration;
             
             this.imageUI = imageUI;
-
-           
         }
 
         public override NodeState Evaluate(){
