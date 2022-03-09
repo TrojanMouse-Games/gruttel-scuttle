28f6edf864a639c915beb6c6f4179d6a5b46ed47
Josh Thompson <jt244345@falmouth.ac.uk>
2022-03-09 15:21:55 +0000

    Commented the new stat code

--- Assets/Scripts/Gameplay Loop/ShowGruttelStats.cs
+++ Assets/Scripts/Gameplay Loop/ShowGruttelStats.cs
@@ -25,9 +25,9 @@ namespace TrojanMouse.GameplayLoop{
             if(Physics.Raycast(ray, out hit, maxDetectionDistance, whatIsGruttel)){ // CHECK TO SEE IF RAY HITS GRUTTEL
                 GruttelReference gruttel = hit.transform.GetComponent<GruttelReference>(); // GETS THE DATA FROM THE GRUTTEL                
 
-                for(int i = 0; i < statsUI.GetChild(0).childCount; i++){
+                for(int i = 0; i < statsUI.GetChild(0).childCount; i++){ // ITERATES THROUGH EACH LABEL OBJECT
                     TextMeshProUGUI label = statsUI.GetChild(0).GetChild(i).GetComponent<TextMeshProUGUI>();                    
-                    switch(i){
+                    switch(i){ // SWITCH/CASE TO SET CORROSPONDING LABEL TO THE TEXT
                         case 0:
                             label.text = $"{gruttel.data.nickname}";
                             break;
@@ -35,8 +35,8 @@ namespace TrojanMouse.GameplayLoop{
                             label.text = $"{gruttel.data.bios[0]}";
                             break;
                         default:
-                            int curTraitVal = (gruttel.data.traits.Length - statsUI.GetChild(0).childCount) + i;                            
-                            if(curTraitVal < 0){
+                            int curTraitVal = (gruttel.data.traits.Length - statsUI.GetChild(0).childCount) + i; // POSSIBLY VERY TEMPERMENTAL -- I WAS TRYING TO FIGURE OUT THE MATHS FOR THIS IN MY HEAD. WHAT ITS SUPPOSE TO DO AND KINDA DOES, IS RESET 'i's ANCHOR POINT AND MAP IT TO THE LENGTH OF TRAITS                            
+                            if(curTraitVal < 0){ // SOMETIMES THE ABOVE VALUE RETURNS A VALUE <0 WHICH WOULD OBVIOUSLY CAUSE AN ERROR, SO WE SANITISE CHECK THAT VALUE
                                 continue;
                             }
                             
