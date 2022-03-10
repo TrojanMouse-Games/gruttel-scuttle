98b75275c010fc28e5e4f616e78dd0228ec03073
Matthew Roberts <mr232432@falmouth.ac.uk>
2022-03-10 17:25:19 +0000

    Optimized Drawbride code, added pivot
    optimized code, added pivot and move colliders to child object.

--- Assets/Scripts/Obstacles/DrawBridge.cs
+++ Assets/Scripts/Obstacles/DrawBridge.cs
@@ -3,102 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class DrawBridge : MonoBehaviour
+namespace TrojanMouse.Game.Obstacles
 {
-    // This is used to show the bridges state
-    public bool isRaised = false;
-
-    // Ray & hit to be used to for raycast
-    public Ray ray;
-    public RaycastHit hit;
-
-    // Navmesh References
-    NavMeshObstacle navMeshObstacle;
-    
-    // Other stuff
-    public Vector3 positionToMoveTo, positionToRotateTo;
-    Vector3 originalRotation, originalPosition;
-
-    /// <summary>
-    /// Start is called on the frame when a script is enabled just before
-    /// any of the Update methods is called the first time.
-    /// </summary>
-    void Start()
+    public class DrawBridge : MonoBehaviour
     {
-        navMeshObstacle = gameObject.AddComponent<NavMeshObstacle>();
-    }
+        // This is used to show the bridges state
+        public bool isRaised = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (CheckForClick())
+        // Navmesh References
+        NavMeshObstacle navMeshObstacle;
+
+        // Other stuff
+        public Vector3 positionToMoveTo, positionToRotateTo;
+        public GameObject pivot;
+        Vector3 originalRotation, originalPosition;
+
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start()
         {
-            // Having the ray pos set in here avoids it being called each frame.
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            navMeshObstacle = gameObject.AddComponent<NavMeshObstacle>();
+        }
 
-            // Fire the ray and check to see if the bridge has been clicked
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity) && hit.transform == this.transform)
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OnMouseDown()
+        {
+            isRaised = !isRaised;
+            Debug.Log($"Hit the drawbridge, its name is {transform.name}! {isRaised}");
+
+            MoveBridge();
+        }
+
+        /// <summary>
+        /// Moves the drawbridge upwards.
+        /// </summary>
+        void MoveBridge()
+        {
+            if (isRaised)
             {
-                isRaised = !isRaised;
-                Debug.Log($"Hit the drawbridge, its name is {hit.transform.name}! {isRaised}");
+                // Save original postion
+                originalPosition = pivot.transform.localPosition;
+                originalRotation = pivot.transform.localRotation.eulerAngles;
 
-                MoveBridge();
+                // Move to upwards pos
+                pivot.transform.localRotation = Quaternion.Euler(positionToRotateTo);
+                pivot.transform.localPosition = positionToMoveTo;
+
+                // Change the mesh
+                ChangeNavMesh();
+            }
+            else
+            {
+                // Move to original pos
+                pivot.transform.localRotation = Quaternion.Euler(originalRotation);
+                pivot.transform.localPosition = originalPosition;
+
+                // Change the mesh
+                ChangeNavMesh();
             }
         }
-    }
 
-    /// <summary>
-    /// Checks for a click from the mouse.
-    /// </summary>
-    /// <returns>True if clicked, else false</returns>
-    bool CheckForClick()
-    {
-        if (Input.GetMouseButtonDown(0))
+        /// <summary>
+        /// Changes the navmesh
+        /// </summary>
+        void ChangeNavMesh()
         {
-            return true;
+            navMeshObstacle.carving = !navMeshObstacle.carving;
         }
-        else
-        {
-            return false;
-        }
-    }
-
-    /// <summary>
-    /// Moves the drawbridge upwards.
-    /// </summary>
-    void MoveBridge()
-    {
-        if (isRaised)
-        {
-            // Save original postion
-            originalPosition = transform.position;
-            originalRotation = transform.rotation.eulerAngles;
-
-            // Move to upwards pos
-            hit.transform.rotation = Quaternion.Euler(positionToRotateTo);
-            hit.transform.position = positionToMoveTo;
-            
-            //Rebake the mesh
-            // Currently DONT do this, freezes the editor, crashes builds and ends the universe
-            ChangeNavMesh();
-        }
-        else
-        {
-            // Move to original pos
-            hit.transform.rotation = Quaternion.Euler(originalRotation);
-            hit.transform.position = originalPosition;
-
-            //Rebake the mesh
-            // Currently DONT do this, freezes the editor, crashes builds and ends the universe
-            ChangeNavMesh();
-        }
-    }
-
-    /// <summary>
-    /// Changes the navmesh
-    /// </summary>
-    void ChangeNavMesh()
-    {
-        navMeshObstacle.carving = !navMeshObstacle.carving;
     }
 }
+
