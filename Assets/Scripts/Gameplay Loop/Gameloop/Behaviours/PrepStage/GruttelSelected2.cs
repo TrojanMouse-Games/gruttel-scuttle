720192c2c7e7392d4c477498c50791238a9a389f
Joshua Thompson <jt244345@falmouth.ac.uk>
2022-03-22 14:00:10 +0000

    Finished programming the new selection system with some tweaks needed
    Just needs the camera to zoom into the Gruttels and interpolate between them all

--- Assets/Scripts/Gameplay Loop/Gameloop/Behaviours/PrepStage/GruttelSelected2.cs
+++ Assets/Scripts/Gameplay Loop/Gameloop/Behaviours/PrepStage/GruttelSelected2.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using TrojanMouse.Gruttel;
 
 namespace TrojanMouse.GameplayLoop
 {
     public class GruttelsSelected2 : GLNode{
         HashSet<Transform> gruttelsSelected = new HashSet<Transform>();
 
-        Transform[] gruttels;
+        int gruttelsToSelect;
         Camera cam;
         float maxDistance;
-        public GruttelsSelected2(Transform[] gruttels, Camera cam, float maxRayDistance){ // CONSTRUCTOR TO PREDEFINE THIS CLASS VARIABLES
-            this.gruttels = gruttels;
+        LayerMask whatIsGruttel;
+        ShowGruttelStats statScript;
+
+        Transform powerupStorage;
+        Transform villageFolder;
+        Transform playFolder;
+        EventReference selectSound;
+        public GruttelsSelected2(int gruttelsToSelect, Camera cam, float maxRayDistance, LayerMask whatIsGruttel, ShowGruttelStats statScript,Transform powerupStorage, Transform villageFolder, Transform playFolder, EventReference selectSound){ // CONSTRUCTOR TO PREDEFINE THIS CLASS VARIABLES
+            this.gruttelsToSelect = gruttelsToSelect;
             this.cam = cam;
-            this.maxDistance = maxRayDistance
+            this.maxDistance = maxRayDistance;
+            this.whatIsGruttel = whatIsGruttel;
+            this.statScript = statScript;
+            this.powerupStorage = powerupStorage;
+            this.villageFolder = villageFolder;
+            this.playFolder = playFolder;
+            this.selectSound = selectSound;
         }
 
         int gruttelSelectedIndex = 0;
@@ -27,24 +41,45 @@ namespace TrojanMouse.GameplayLoop
             );
 
             if(gruttelSelectedIndex < 0){
-                gruttelSelectedIndex = gruttels.Length;
+                gruttelSelectedIndex = villageFolder.childCount-1;
             }
-            else if(gruttelSelectedIndex > gruttels.Length){
+            else if(gruttelSelectedIndex > villageFolder.childCount-1){
                 gruttelSelectedIndex = 0;
             }
+
+            GruttelReference gruttel = villageFolder.GetChild(gruttelSelectedIndex).GetComponent<GruttelReference>();
+            // MOVE CAMERA
             #endregion
 
             #region GRUTTEL SELECTION
             if (Input.GetMouseButtonDown(0)){
                 // CHECK IF GRUTTEL IS CLICKED ON
                 RaycastHit hit;
-                if (Physics.Raycast(GameLoopBT.instance.GetMouse(cam), out hit, maxDistance, whatIsGruttel)){ // FIRES A RAYCAST FROM WHEN THE USER CLICKS
-                  // CHECK IF POWERUP IS APPLIED
+                if (Physics.Raycast(GameLoopBT.instance.GetMouse(cam), out hit, maxDistance, whatIsGruttel)){ // FIRES A RAYCAST FROM WHEN THE USER CLICKS                    
+                    if(hit.transform.GetInstanceID() != gruttel.transform.GetInstanceID() || gruttel.data.type != GruttelType.Normal){ // CHECK IF POWERUP IS ALREADY APPLIED AKA GRUTTEL IS LOCKED IN
+                        return NodeState.FAILURE;
+                    }
+
+                    if (!gruttelsSelected.Contains(hit.collider.transform) && gruttelsSelected.Count < gruttelsToSelect){
+                        gruttelsSelected.Add(hit.collider.transform);
+                        hit.collider.transform.localScale = Vector3.one * 1.15f;
+                        RuntimeManager.PlayOneShot(selectSound);
+                    }
+                    else if(gruttelsSelected.Contains(hit.collider.transform)){
+                        gruttelsSelected.Remove(hit.collider.transform);
+                        hit.collider.transform.localScale = Vector3.one;
+                    }                    
                 }
+            }
+            #endregion
+            
+            if (powerupStorage.parent.GetComponentsInChildren<Powerup>().Length <= 0 && gruttelsSelected.Count >= gruttelsToSelect){ // THIS COULD BE SET TO || IF THE USER SHOULDNT BE FORCED TO USE ALL POWERUPS
+                return NodeState.SUCCESS;
             }
 
 
-
+            statScript.UpdateStats(gruttel);
+            statScript.EnableUI(true);
             return NodeState.FAILURE;
         }
     }
--- Assets/Scripts/Gameplay Loop/Gameloop/GameLoopBT.cs
+++ Assets/Scripts/Gameplay Loop/Gameloop/GameLoopBT.cs
@@ -27,21 +27,20 @@ namespace TrojanMouse.GameplayLoop{
             #region NODES
             #region PREP NODES
             SpawnGruttels spawnGruttels = new SpawnGruttels(prerequisiteSettings.gruttelObj, prerequisiteSettings.gruttelVillageSpawnPoints, prerequisiteSettings.objectForGruttelsToLookAtWhenSpawned, prerequisiteSettings.gruttelVillageFolder); // PASS IN BOTH FOLDERS, IF BOTH EMPTY, THEN SPAWN GRUTTELS
-            GruttelsSelected areGruttelsSelected = new GruttelsSelected(level.numOfGruttelsToSelect, 100, prerequisiteSettings.whatIsGruttel, cam, prerequisiteSettings.gruttelVillageFolder, prerequisiteSettings.gruttelPlayFolder, prerequisiteSettings.selectSound);
+            //GruttelsSelected areGruttelsSelected = new GruttelsSelected(level.numOfGruttelsToSelect, 100, prerequisiteSettings.whatIsGruttel, cam, prerequisiteSettings.gruttelVillageFolder, prerequisiteSettings.gruttelPlayFolder, prerequisiteSettings.selectSound);
             SpawnPowerups spawnPowerups = new SpawnPowerups(prerequisiteSettings.powerupPrefab, level.powerups, prerequisiteSettings.powerupSpawnFolder);
+            GruttelsSelected2 areGruttelsSelected = new GruttelsSelected2(level.numOfGruttelsToSelect, cam, 100, prerequisiteSettings.whatIsGruttel, prerequisiteSettings.statScript, prerequisiteSettings.powerupSpawnFolder, prerequisiteSettings.gruttelVillageFolder, prerequisiteSettings.gruttelPlayFolder, prerequisiteSettings.selectSound);
             PowerupsUsed arePowerupsUsed = new PowerupsUsed(prerequisiteSettings.powerupSpawnFolder);
             ChangeUIText selectGruttelsText = new ChangeUIText(prerequisiteSettings.tipText, $"Click on {level.numOfGruttelsToSelect} Gruttels to proceed");
             ChangeUIText addPowerupsText = new ChangeUIText(prerequisiteSettings.tipText, $"Drag and drop Nana Betsy's onto your Gruttels");
             EnableAI disableAI = new EnableAI(EnableAI.AIState.Disabled);
-            ChangeCamera prepCam = new ChangeCamera(prerequisiteSettings.prepCamera, cameras);
-            EnableGruttelStats enableStats = new EnableGruttelStats(prerequisiteSettings.statScript, true);
+            ChangeCamera prepCam = new ChangeCamera(prerequisiteSettings.prepCamera, cameras);            
             #endregion
             #region READY NODES
             ChangeUIText dragGruttelsText = new ChangeUIText(prerequisiteSettings.tipText, $"Drag and drop Gruttels into position before the game starts!");
             ChangeCamera readyCam = new ChangeCamera(prerequisiteSettings.readyStageCamera, cameras);
             Intermission timeToDragGruttels = new Intermission(level.readyStageIntermission, prerequisiteSettings.intermissionTimer, prerequisiteSettings.timerLabel);
-            EnableAI dragAI = new EnableAI(EnableAI.AIState.Dragable);
-            EnableGruttelStats disableStats = new EnableGruttelStats(prerequisiteSettings.statScript, false);
+            EnableAI dragAI = new EnableAI(EnableAI.AIState.Dragable);            
             #endregion
             #region MAIN NODES
             ChangeUIText mainRoundText = new ChangeUIText(prerequisiteSettings.tipText, $"Round started, Click on the Gruttels and guide them to litter!");
@@ -64,13 +63,9 @@ namespace TrojanMouse.GameplayLoop{
                 spawnGruttels, 
                 prepCam, 
                 selectGruttelsText, 
-                disableAI, 
-                enableStats, 
-                areGruttelsSelected,
-                disableStats, 
-                spawnPowerups, 
-                addPowerupsText, 
-                arePowerupsUsed
+                disableAI,                 
+                spawnPowerups,                 
+                areGruttelsSelected
             });
             #endregion
             #region READYSTAGE
