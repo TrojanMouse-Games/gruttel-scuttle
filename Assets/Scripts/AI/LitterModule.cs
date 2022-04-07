87b62438a9449889a473b78500e7b6b9ef98bee7
JOSH THOMPSON <jt244345@falmouth.ac.uk>
2022-04-07 16:41:14 +0100

    Litter is now prioritised based on type

--- Assets/Scripts/AI/LitterModule.cs
+++ Assets/Scripts/AI/LitterModule.cs
@@ -34,6 +34,7 @@ namespace TrojanMouse.AI
 
         public LitterObjectHolder GetNewTarget(AIData data, Inventory.Inventory inventory)
         {
+            LitterObjectHolder potentialTarget = null;
             if (inventory.HasSlotsLeft())
             {
                 Collider[] litterInRange = Physics.OverlapSphere(transform.position, data.detectionRadius, data.litterLayer);
@@ -42,16 +43,18 @@ namespace TrojanMouse.AI
                 foreach (Collider c in litterInRange)
                 {
                     LitterObjectHolder litter = c.GetComponent<LitterObjectHolder>();
-
                     bool canPickup = CanPickupLitter(litter.litterObject.typeOfLitter, gruttelType);
                     Debug.Log($"can pickup: {canPickup}");
 
-                    if (canPickup)
-                    {
+                    if (canPickup){
                         data.agent.SetDestination(litter.transform.position);
-                        return litter;
+                        if(litter.litterObject.typeOfLitter == gruttelType){ // SHOULD MEAN IF THE LITTER IS THE SAME TYPE OF THE BUFFED GRUTTEL THEN THE GRUTTEL WILL PRIORITISE THE SAME TYPE OF LITTER
+                            return litter;
+                        }
+                        potentialTarget = litter;
                     }
                 }
+                return potentialTarget;
             }
 
             return null;
