5b10215701667176d1d2481882296f293059d0e5
Cassy <EB247426@falmouth.ac.uk>
2022-04-08 02:19:10 +0100

    Started work on the star rating system for progression, mainly on calculating the rating at this stage.
    -In theory it should be adding the current stress level to a list the same time it updates the stress bar, and once you win the game (or press the handy debug button), it calculates your average stress level across the game and give you a star rating based on thresholds (so currently you get 3 stars if you were below 30% stress on average etc).
    - The thresholds are customisable in the level scriptable object.
    - I think it works but I'm not sure if its adding correct values to the list, will check with programmers soon.
    - This is very messy and has a lot of debug statements, just committing my progress for now.
    - Currently doesn't do any further than the calculation, still to come is it being presented on victory screen and being saved as a score and high score for each level.

--- Assets/Scripts/CurrenciesAndRewards/CurrenciesAndValues.cs
+++ Assets/Scripts/CurrenciesAndRewards/CurrenciesAndValues.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TrojanMouse.StressSystem;
+using TrojanMouse.GameplayLoop;
+using System.Linq;
 public class CurrenciesAndValues : MonoBehaviour
 {
     //NPC items - Can be used to unlock special NPCs - gets added to when reward gained.
@@ -16,6 +19,15 @@ public class CurrenciesAndValues : MonoBehaviour
     public Image currencySprite;
     //UI text to change
     public Text currencyText;
+    //region handler object to access stress and levels
+    public GameObject regionHandler;
+    public float averageStressAmount;
+
+    public float maxStress;
+    public float stressPercent;
+    public float firstStressThreshold;
+    public float secondStressThreshold;
+
 
     // Start is called before the first frame update
     void Start()
@@ -46,5 +58,47 @@ public class CurrenciesAndValues : MonoBehaviour
         currencySprite.gameObject.SetActive(false);
         currencyText.gameObject.SetActive(false);
         currencyText.text = "";
+    }
+    //called on victory but before victory screen
+    public void StarRating()
+    {
+        //Gets the list of stress values each second from the stress system
+        List<float> levelStressValues = Stress.current.levelStressValues;
+        Debug.Log("levelStressValues list ");
+        foreach (float i in levelStressValues)
+        {
+            Debug.Log(i.ToString());
+        }
+        averageStressAmount = levelStressValues.Average();
+        Debug.Log("average stress amount" + averageStressAmount);
+        float curLevel = regionHandler.GetComponent<GameLoopBT>().curLevel;
+        Debug.Log("current level object name " + regionHandler.GetComponent<GameLoopBT>().levels[(int)curLevel].name);
+        Vector2 stressThresholds = regionHandler.GetComponent<GameLoopBT>().levels[(int)curLevel].stressThresholds;
+        maxStress = Stress.current.maxLitter;
+        Debug.Log("max stress" + maxStress);
+        stressPercent = (averageStressAmount / maxStress)*100;
+        Debug.Log("stress percent " + stressPercent);
+        firstStressThreshold = stressThresholds.x;
+        secondStressThreshold = stressThresholds.y;
+        Debug.Log("stress thresholds are " + firstStressThreshold + " and " + secondStressThreshold);
+        if (0 < stressPercent && stressPercent <= firstStressThreshold)
+        {
+            //3 stars
+            Debug.Log("you get 3 stars!");
+        }
+        else if (firstStressThreshold < stressPercent && stressPercent <= secondStressThreshold)
+        {
+            //2 stars
+            Debug.Log("you get 2 stars!");
+        }
+        else if (secondStressThreshold < stressPercent && stressPercent < maxStress)
+        {
+            //1 stars
+            Debug.Log("you get 1 stars!");
+        }
+        else
+        {
+            Debug.LogError("Cassy has fucked up with the star rating.");
+        }
     }
 }
