fad7891d4b92e26c624fc52dff3b60bb9be4082f
mr232432 <mr232432@falmouth.ac.uk>
2022-05-08 18:26:55 +0100

    Got pickup animation for gruttels working again
    Brought over the correct version of the drawbridge script, minor semicircle scene changes

--- Assets/Scripts/AI/Movement Modules/MoveWithMouseGrab.cs
+++ Assets/Scripts/AI/Movement Modules/MoveWithMouseGrab.cs
@@ -118,11 +118,13 @@ namespace TrojanMouse.AI.Movement
                 if (type == "pickUp" && controller)
                 {
                     data.agent.enabled = state;
+                    controller.animator.SetBool("isPickedUp", true);
                     controller.enabled = state;
                 }
                 else if (type == "putDown" && controller)
                 {
                     controller.enabled = state;
+                    controller.animator.SetBool("isPickedUp", false);
                     data.agent.enabled = state;
                     controller.moduleManager.litterModule.GetLitter(data);
                 }
