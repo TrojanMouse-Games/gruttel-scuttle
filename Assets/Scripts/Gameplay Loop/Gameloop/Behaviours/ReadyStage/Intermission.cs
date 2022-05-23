19dd2d7dc3e622ca01c98b456a24e7fb24b25329
Hayley Davies <cd230099@falmouth.ac.uk>
2022-05-23 01:55:23 +0100

    Added skip button for intermission

--- Assets/Scripts/Gameplay Loop/Gameloop/Behaviours/ReadyStage/Intermission.cs
+++ Assets/Scripts/Gameplay Loop/Gameloop/Behaviours/ReadyStage/Intermission.cs
@@ -5,33 +5,49 @@ using UnityEngine.UI;
 using TMPro;
 using FMODUnity;
 
-namespace TrojanMouse.GameplayLoop{ 
-    public class Intermission : GLNode{
+namespace TrojanMouse.GameplayLoop
+{
+    public class Intermission : GLNode
+    {
         public float maxDuration, remainingDuration;
         public Image imageUI;
         TextMeshProUGUI label;
-        
-        public Intermission(float duration, Image imageUI = null, TextMeshProUGUI label = null){ // CONSTRUCTOR TO PREDEFINE THIS CLASS VARIABLES
+        public Button skipButton;
+
+        public Intermission(float duration, Image imageUI = null, TextMeshProUGUI label = null, Button skipButton = null)
+        { // CONSTRUCTOR TO PREDEFINE THIS CLASS VARIABLES
             this.maxDuration = duration;
             this.remainingDuration = duration;
-            
+
             this.imageUI = imageUI;
             this.label = label;
-         
+            this.skipButton = skipButton;
+            if (skipButton)
+                skipButton.gameObject.SetActive(false);
         }
 
-        public override NodeState Evaluate(){
+        public override NodeState Evaluate()
+        {
             #region DEPENDANCY MANAGEMENT
-            if (imageUI){
+            if (imageUI)
+            {
                 imageUI.fillAmount = (remainingDuration / maxDuration); // FILLS THE IMAGE BASED ON THE REMAINING DURATION
             }
-            if(label){
+            if (label)
+            {
                 label.text = Mathf.Ceil(remainingDuration).ToString(); // CONVERTS THE REMAINING DURATION VALUE TO A STRING FOR THE TEXT LABEL
             }
+            if (skipButton)
+            {
+                skipButton.gameObject.SetActive(true);
+            }
             #endregion
-            if (remainingDuration <= 0){
+            if (remainingDuration <= 0)
+            {
                 imageUI?.transform.parent.gameObject.SetActive(false); // DISABLES THE TIMER
-                
+                if (skipButton)
+                    skipButton.gameObject.SetActive(false);
+
                 return NodeState.SUCCESS;
 
             }
